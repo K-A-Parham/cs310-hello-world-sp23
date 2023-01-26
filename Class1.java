f76a761e601119edce690e14206e6783a23fa32e
KParham1 <kparham1@stu.jsu.edu>
2023-01-25 21:04:04 -0600

    Cleaning Up Spaces

---
+++ Class1.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Kaleb Parham
+ */
+
+public class Class1 {
+
+    private void f1() {
+        // v1.0
+    }
+    private void f2() {
+        // v2.0
+    }
+    private void f3() {
+        // v3.0
+    }
+}
